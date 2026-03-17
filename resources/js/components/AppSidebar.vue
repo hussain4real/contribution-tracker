2f616108433ef02a76bdf7ceccafeaa872a032dc
Hussain Aminu <hussain_aminu@yahoo.com>
2026-03-17 17:51:30 +0300

    style: comment out unused footer navigation items in AppSidebar.vue

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -92,16 +92,16 @@ const mainNavItems = computed<NavItem[]>(() => {
 });
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
+    // {
+    //     title: 'Github Repo',
+    //     href: 'https://github.com/laravel/vue-starter-kit',
+    //     icon: Folder,
+    // },
+    // {
+    //     title: 'Documentation',
+    //     href: 'https://laravel.com/docs/starter-kits#vue',
+    //     icon: BookOpen,
+    // },
 ];
 </script>
 
