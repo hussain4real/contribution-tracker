90f04306f4a4c15fea5143fcbd41a5591eec34f8
Hussain Aminu <hussain_aminu@yahoo.com>
2026-07-02 23:12:10 +0300

    Restore globals in method override sanitizer test

--- tests/Unit/HttpMethodOverrideSanitizerTest.php
+++ tests/Unit/HttpMethodOverrideSanitizerTest.php
@@ -6,11 +6,18 @@ use App\Http\HttpMethodOverrideSanitizer;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException;
 
+beforeEach(function () {
+    $this->originalGet = $_GET;
+    $this->originalPost = $_POST;
+    $this->originalRequest = $_REQUEST;
+    $this->originalServer = $_SERVER;
+});
+
 afterEach(function () {
-    $_GET = [];
-    $_POST = [];
-    $_REQUEST = [];
-    $_SERVER = [];
+    $_GET = $this->originalGet;
+    $_POST = $this->originalPost;
+    $_REQUEST = $this->originalRequest;
+    $_SERVER = $this->originalServer;
 });
 
 it('removes unsupported form method overrides before the request is captured', function () {
